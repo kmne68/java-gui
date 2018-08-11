8a4300035430297379ab36a62bc580868fc567c9
Keith Emery <kemery@pnmg.com>
2018-08-10 19:02:21 -0500

    Initial commit

---
+++ src/gui/GameBoard.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+// import javax.swing.AbstractAction.*;
+
+import javax.swing.JFrame;
+
+
+/**
+ *
+ * @author Keith
+ */
+public class GameBoard extends JFrame {
+    
+    public GameBoard() {
+        
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.getContentPane().setSize(800, 800);
+        this.setTitle("Game GUI");
+        this.setBounds(250, 250, 800, 800);
+        this.setVisible(true);
+    }
+}
