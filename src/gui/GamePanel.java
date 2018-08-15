38d9ea38e3ad7b953c98de8ae4026390258931c3
Keith Emery <kmne68@gmail.com>
2018-08-15 00:06:43 -0500

    WIP continue adding action listeners

--- src/gui/GamePanel.java
+++ src/gui/GamePanel.java
@@ -15,8 +15,8 @@ import javax.swing.*;
  */
 public class GamePanel extends JPanel {
 
-    private JLabel field1;
-    private JLabel field2;
+    private JLabel lblField1;
+    private JLabel lblField2;
     private JLabel resultLabel;
 
     private JTextField textField1;  // 10 char wide
@@ -30,10 +30,10 @@ public class GamePanel extends JPanel {
 
     private final String field1LabelName = "Field one label";
     private final String field2LabelName = "Field two label";
-    private final String field1NextName = "next";
-    private final String field1FirstName = "first";
-    private final String field1PreviousName = "previous";
-    private final String field1LastName = "last";
+    final String btnNextName = "next";
+    final String btnFirstName = "first";
+    final String btnPreviousName = "previous";
+    final String btnLastName = "last";
 
     int clickCount1;
     int clickCount2;
@@ -43,8 +43,12 @@ public class GamePanel extends JPanel {
 
         this.setSize(400, 400);
 
-        field1 = new JLabel("Enter input 1:");
-        field2 = new JLabel("Enter input 2:");
+        
+        lblField1 = new JLabel("Enter input 1:");
+        lblField1.setName(field1LabelName);
+        
+        lblField2 = new JLabel("Enter input 2:");
+        lblField2.setName(field2LabelName);
 
         textField1 = new JTextField(10); // 10 char wide
         textField2 = new JTextField(10);
@@ -53,27 +57,62 @@ public class GamePanel extends JPanel {
         button1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                
+                boolean enabled = false;
+                String message = "";
+                
+                switch(clickCount % 2)
+                {
+                    case 0:
+                        message = "All set";
+                        enabled = false;
+                        button1.setText("Unlock");
+                        break;
+                    case 1:
+                        message = "Unlocked";
+                        enabled = true;
+                        button1.setText("Save input");
+                        break;
+                    default:
+                        break;
+                }
+                
+                textField1.setEnabled(enabled);
+                textField2.setEnabled(enabled);
+                
+                JOptionPane.showMessageDialog(null, message, "Fields", JOptionPane.INFORMATION_MESSAGE);
+                clickCount++;
             }
            
             
         });
         
+        ActionListener navButtonEvent = new NavigationButtonPressEvent();
         button2 = new JButton(" << first ");
-        button3 = new JButton(" < prev ");
-        button4 = new JButton(" > next ");
-        button5 = new JButton(" >> last ");       
+        button2.setName(btnFirstName);
+        button2.addActionListener(navButtonEvent);
         
-        this.add(field1);
+        button3 = new JButton(" < prev ");
+        button3.setName(btnPreviousName);
+        button3.addActionListener(navButtonEvent);
+        
+        button4 = new JButton(" > next ");
+        button4.setName(btnNextName);
+        button4.addActionListener(navButtonEvent);
+        
+        button5 = new JButton(" >> last "); 
+        button5.setName(btnLastName);
+        button5.addActionListener(navButtonEvent);
+        
+        this.add(lblField1);
 
         this.add(textField1);
 
-        this.add(field2);
+        this.add(lblField2);
 
         this.add(textField2);
 
-        this.add(button1);
-        
+        this.add(button1);        
         this.add(button2);
         this.add(button3);
         this.add(button4);
@@ -85,4 +124,43 @@ public class GamePanel extends JPanel {
 
         this.setVisible(true);
     }
+    
+    
+    class NavigationButtonPressEvent implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            
+            Object objectSource = e.getSource();
+            if(objectSource instanceof JButton) {
+                
+                JButton button = (JButton) objectSource;
+                String buttonName = button.getName();
+                String message = "";
+                
+                if(buttonName.equals(btnFirstName)) {
+                    message = "Button First pressed.";
+                    System.out.println(buttonName);
+                    
+                }
+                else if(buttonName.equals(btnLastName)) {
+                    message = "Button Last pressed.";
+                    System.out.println(buttonName);
+                    
+                }
+                else if(buttonName.equals(btnPreviousName)) {
+                    message = "Button Previous pressed.";
+                    System.out.println(buttonName);
+                    
+                } 
+                else if(buttonName.equals(btnNextName)) {
+                    message = "Button Next pressed.";
+                    System.out.println(buttonName);
+                }
+                resultLabel.setText(message);
+            }
+            
+        }
+    
+}
 }
